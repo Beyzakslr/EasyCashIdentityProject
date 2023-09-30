6df93878deda62eeae5b5be5fd437c09d1c88c6c
Beyza Kesler <beyzakslr@hotmail.com>
2023-09-30 16:50:48 +0300

    21. Ders Sonu

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/MyAccountsController.cs
@@ -0,0 +1,36 @@
+﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    [Authorize]
+    public class MyAccountsController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public MyAccountsController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            AppUserEditDto appUserEditDto = new AppUserEditDto();
+            appUserEditDto.Name = values.Name;
+            appUserEditDto.Surname = values.Surname;
+            appUserEditDto.PhoneNumber = values.PhoneNumber;
+            appUserEditDto.Email = values.Email;
+            appUserEditDto.City = values.City;
+            appUserEditDto.District = values.District;
+            appUserEditDto.ImageUrl = values.ImageUrl;
+           
+            return View(appUserEditDto);
+        }
+    }
+}
---
+++ EasyCashIdentityProject.PresentationLayer/ViewComponents/Customer/CustomerLayoutNavbarPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace EasyCashIdentityProject.PresentationLayer.ViewComponents.Customer
+{
+    public class _CustomerLayoutNavbarPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
